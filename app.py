275e738af9d95e864aa01601cd8120ab52418a9e
kianooshes <167688399+kianooshes@users.noreply.github.com>
2025-05-05 17:37:01 +0330

    aawqeqwe

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from datetime import datetime
 import os
 
@@ -8,29 +8,31 @@ last_data = {}
 
 @app.route('/')
 def index():
-    return "Server is up and running!"
+    return render_template('index.html')
 
 @app.route('/iot-data', methods=['POST'])
 def receive_data():
     global last_data
-    data = request.get_json()  
+    data = request.get_json()
 
-    if 'ID' not in data or 'MCC' not in data or 'MNC' not in data or 'LAC' not in data:
-        return jsonify({'status': 'error', 'message': 'Missing data fields'}), 400
+    if 'lat' in data:
+        data['latitude'] = data.pop('lat')
+    if 'lon' in data:
+        data['longitude'] = data.pop('lon')
 
     data['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
     last_data = data
+    return jsonify({'status': 'success', 'message': 'Data received', 'data': data})
 
-    return jsonify({'status': 'success', 'message': 'Data received', 'data': data}), 200
 
-@app.route('/latest', methods=['GET'])
+@app.route('/latest')
 def latest_data():
     if last_data:
-        return jsonify(last_data), 200
+        return jsonify(last_data)
     else:
-        return jsonify({'status': 'error', 'message': 'No data available'}), 404
+        return jsonify({'latitude': None, 'longitude': None, 'timestamp': None})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
