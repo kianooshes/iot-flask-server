43d0b661fd774b5f87689b2ea94f244988c3e72a
kianooshes <167688399+kianooshes@users.noreply.github.com>
2025-05-05 11:58:34 +0330

    change

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ import os
 
 app = Flask(__name__)
 
-last_data = {}
+cell_data = {}
+
+def load_cell_data():
+    global cell_data
+    with open('cell_data.txt', 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            cell_type, mcc, mnc, lac, cell_id, _, lon, lat, *_ = parts
+            key = (cell_id, mcc, mnc, lac)  
+            cell_data[key] = {'lat': float(lat), 'lon': float(lon)}
 
 @app.route('/')
 def index():
@@ -12,27 +21,25 @@ def index():
 
 @app.route('/iot-data', methods=['POST'])
 def receive_data():
-    global last_data
     data = request.get_json()
 
-    if 'lat' in data:
-        data['latitude'] = data.pop('lat')
-    if 'lon' in data:
-        data['longitude'] = data.pop('lon')
+    ids = data.get('ID', [])
+    mcc = data.get('MCC', [])
+    mnc = data.get('MNC', [])
+    lac = data.get('LAC', [])
 
-    data['timestamp'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    last_data = data
-    return jsonify({'status': 'success', 'message': 'Data received', 'data': data})
+    updated_cells = []
+    for i in range(len(ids)):
+        cell_key = (str(ids[i]), str(mcc[i]), str(mnc[i]), str(lac[i]))
+        if cell_key in cell_data:
+            updated_cells.append(cell_data[cell_key])
 
+    return jsonify({'status': 'success', 'message': 'Data received', 'cells': updated_cells})
 
 @app.route('/latest')
 def latest_data():
-    if last_data:
-        return jsonify(last_data)
-    else:
-        return jsonify({'latitude': None, 'longitude': None, 'timestamp': None})
+    return jsonify(cell_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    load_cell_data()
+    app.run(host='0.0.0.0', port=5000)
